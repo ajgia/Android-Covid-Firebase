28f4cc15f024f8ecc4566cb347c74dd661935bf7
Alex G <giasson.alexj@gmail.com>
2021-11-07 21:00:42 -0800

    gender frag done

--- app/src/main/java/com/example/assignment2_covidfirebase/GenderFragment.java
+++ app/src/main/java/com/example/assignment2_covidfirebase/GenderFragment.java
@@ -1,11 +1,16 @@
 package com.example.assignment2_covidfirebase;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import java.util.ArrayList;
@@ -29,8 +34,49 @@ public class GenderFragment extends Fragment {
             map.merge(key, 1, Integer::sum);
         }
 
+        ArrayList<String> resultList = new ArrayList<String>();
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String k = entry.getKey();
+            String v = entry.getValue().toString();
+            String listEntry = k + " : " + v;
+            resultList.add(listEntry);
+        }
+
+        lvAge = view.findViewById(R.id.lvAge);
+        GenderListAdapter adapter = new GenderListAdapter(getActivity(), resultList);
+        lvAge.setAdapter(adapter);
+
 
         return view;
+    }
+
+    public class GenderListAdapter extends ArrayAdapter<String> {
+        private Activity context;
+        private List<String> genderList;
+
+        public GenderListAdapter(Activity context, List<String> genderList) {
+            super(context, R.layout.list_layout, genderList);
+            this.context = context;
+            this.genderList = genderList;
+        }
+
+        public GenderListAdapter(Context context, int resource, List<String> objects, Activity context1, List<String> genderList) {
+            super(context, resource, objects);
+            this.context = context1;
+            this.genderList = genderList;
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = context.getLayoutInflater();
+            View listViewItem = inflater.inflate(R.layout.list_layout_gender, null, true);
+            TextView tvName = listViewItem.findViewById(R.id.textViewGender);
+            String genderString = genderList.get(position);
+            tvName.setText(genderString);
+            return listViewItem;
+        }
+
     }
 
 
